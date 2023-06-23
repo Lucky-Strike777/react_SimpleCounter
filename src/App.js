a06aa3ccbfa23a00f49add108127ec0150e129b7
MinJongKim <minjong_94@naver.com>
2023-06-23 09:39:20 +0900

    no message

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
 import "./App.css";
 import Controller from "./component/Controller";
 import Viewer from "./component/Viewer";
-import { useState } from "react";
+import { useRef, useEffect, useState } from "react";
+import Even from "./component/Even";
 
 function App() {
-const [count, setCount] = useState(0);
-const handleSetCount = (value) => {
-  setCount(count + value);
-};
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  const handleSetCount = (value) => {
+    setCount(count + value);  
+  };
+
+  const handleChangeText = (e)  =>{
+    setText(e.target.value);
+  }
+
+  const didMountRef = useRef(false);
+
+  // useEffect(() => {
+  //   if(!didMountRef.current){
+  //     didMountRef.current = true;
+  //     return;
+  //   } else{
+  //   console.log("컴포넌트 업데이트");
+  //   }
+  // });
+ 
+  // useEffect(() => {
+  //   const intervalID =setInterval(() => {
+  //     console.log("깜빡");
+  //   }, 1000);
+
+  //   return () =>{
+  //     console.log("클린업");
+  //     clearInterval(intervalID);
+  //   }
+  // });
 
   return (
     <div className="App">
       <h1>Simple Counter</h1>
       <section>
-        <Viewer count={count}/>
+        <input value={text} onChange={handleChangeText} />
       </section>
       <section>
-        <Controller handleSetCount={handleSetCount}/>
+        <Viewer count = {count} />
+        {count % 2 === 0 && <Even />}
+      </section>
+      <section>
+        <Controller handleSetCount={handleSetCount} />
       </section>
     </div>
   );
